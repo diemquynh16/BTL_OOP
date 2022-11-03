488319f7366532485ec024406115254547e16d1a
diemquynh16 <6251071079@st.utc2.edu.vn>
2022-11-03 14:17:32 +0700

    .

---
+++ btl/GotoXY.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+void gotoxy(int column, int line);
+
+int move();
+
+string Tien(string str); //them dau '.' vao sau 3 chu so
---
+++ btl/QuanLyDanhMuc.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <iostream>
+#include "MotorbikeBrand.h"
+#include "Date.h"
+#include "Motobike.h"
+#include <string>
+#include "GotoXY.h"
+#include"Tokenizer.h"
+#include<fstream>
+using namespace std;
+
+
+class QuanLyDanhMuc {
+private:
+	vector<MotorbikeBrand> _brands;
+public:
+	void writeDataToFile();
+	vector<MotorbikeBrand> getBrand();
+
+	void themTenHangXe(MotorbikeBrand brand);
+
+	void SuaTenHangXe(string tenHangcu, string tenHangmoi);
+
+	void XoaTenHangXe(string tenHang);
+
+	bool isBrand(string tenHang);
+
+	void DanhSachCacHangXe();
+
+	void XemCacSanPham(string tenhang);
+
+	void themXeMay(XeMay* xemay);
+
+	void xoaXeMay(string tenhang, string tenxe);
+	void suamotXemay();
+
+	XeMay* TimKiemMotXeMay(string tenhang, string tenXe);
+
+	void SanPhamSapHet(); // số lượng < 10
+
+};
